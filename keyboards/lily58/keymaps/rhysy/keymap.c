495a2fea65f4c86a03c5d301f8dc8764cbcbbf24
Rhys Yamada <ryamada@Rhyss-MacBook-Air.local>
2019-09-02 10:06:17 -1000

    Moved tapdance equals from dash to tilde

--- keyboards/lily58/keymaps/rhysy/keymap.c
+++ keyboards/lily58/keymaps/rhysy/keymap.c
@@ -18,14 +18,14 @@ extern rgblight_config_t rgblight_config;
 
 //Tap Dance Declarations
 enum {
-  TD_DASH_EQUALS = 0,
+  TD_GRV_EQUALS = 0,
   TD_SHIFT_CAPS
 };
 
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {  
   // Tap once for Dash, twice for Equals	
-  [TD_DASH_EQUALS]  = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_PEQL),
+  [TD_GRV_EQUALS]  = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_PEQL),
   // Tap once for Shift, twice for Caps Lock
   [TD_SHIFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS)
 // Other declarations would go here, separated by commas, if you have them
@@ -46,9 +46,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | `|=  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | -|=  |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
@@ -59,8 +59,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
  [_QWERTY] = LAYOUT(
-			KC_ESC,  KC_1,	KC_2,   KC_3,   KC_4,   KC_5,                  		KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_GRV, \
-		   	KC_TAB,  KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 						KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, 	 TD(TD_DASH_EQUALS), \
+			KC_ESC,  KC_1,	KC_2,   KC_3,   KC_4,   KC_5,                  		KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    TD(TD_GRV_EQUALS), \
+		   	KC_TAB,  KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 						KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, 	 KC_MINS, \
 		   	KC_LCTL, KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 						KC_H, 	KC_J, 	KC_K, 	KC_L, 	KC_SCLN, KC_QUOT, \
    TD(TD_SHIFT_CAPS),KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B,	KC_LBRC,KC_RBRC, 	KC_N, 	KC_M, 	KC_COMM,KC_DOT, KC_SLSH, KC_RSFT, \
    								KC_LALT, KC_LGUI, MO(1), KC_ENT, 			KC_SPC, KC_BSPC, TG(1), KC_BSLS \
