43a900dd5cba112a8e00f79ad69b4bbe3cfa6bf4
ryamada <ryamada@switchfly.com>
2019-10-01 17:26:58 -1000

    Added CTRL mod taps to home row

--- keyboards/lily58/keymaps/rhysy/keymap.c
+++ keyboards/lily58/keymaps/rhysy/keymap.c
@@ -69,11 +69,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define L_SFT      SFT_T(KC_L)
 #define ESC_GRV    TD(TD_ESC_GRV)
 #define QUOT_CTRL  CTL_T(KC_QUOT)
+#define A_CTL      CTL_T(KC_A)
+#define SCLN_CTL      CTL_T(KC_SCLN)
 
  [_QWERTY] = LAYOUT(
 	ESC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,    KC_5,                  		KC_6,   KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, \
 	KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,    KC_T, 						KC_Y, 	KC_U, 	KC_I, 	 KC_O, 	 KC_P, 	  KC_BSLS, \
-	KC_LCTL,  KC_A,   S_SFT,  D_ALT,  F_CMD,   KC_G, 						KC_H, 	J_CMD, 	K_ALT, 	 L_SFT,  KC_SCLN, QUOT_CTRL, \
+	KC_LCTL,  A_CTL,  S_SFT,  D_ALT,  F_CMD,   KC_G, 						KC_H, 	J_CMD, 	K_ALT, 	 L_SFT,  SCLN_CTL, QUOT_CTRL, \
     SFT_CAPS, KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,   KC_LBRC,  KC_RBRC, 	KC_N, 	KC_M, 	KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
    						KC_LALT, KC_LGUI, TT(1), KC_ENT, 			KC_SPC, KC_BSPC, TT(2), KC_EQL \
  ),
