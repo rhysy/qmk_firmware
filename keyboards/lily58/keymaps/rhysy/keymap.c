77d4a5aa33ae62f4c48f2608c0faf0101a2cc7ac
ryamada <ryamada@switchfly.com>
2019-11-22 11:05:14 -1000

    Spaces

--- keyboards/lily58/keymaps/rhysy/keymap.c
+++ keyboards/lily58/keymaps/rhysy/keymap.c
@@ -73,10 +73,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define SCLN_CTL      CTL_T(KC_SCLN)
 
  [_QWERTY] = LAYOUT(
-	ESC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,    KC_5,                  		KC_6,   KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, \
-	KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,    KC_T, 						KC_Y, 	KC_U, 	KC_I, 	 KC_O, 	 KC_P, 	  KC_BSLS, \
+	ESC_GRV,   KC_1,   KC_2,   KC_3,   KC_4,    KC_5,                  		KC_6,   KC_7,   KC_8,    KC_9,   KC_0,     KC_MINS, \
+	KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,    KC_T, 						KC_Y, 	KC_U, 	KC_I, 	 KC_O, 	 KC_P, 	   KC_BSLS, \
 	KC_LCTL,  A_CTL,  S_SFT,  D_ALT,  F_CMD,   KC_G, 						KC_H, 	J_CMD, 	K_ALT, 	 L_SFT,  SCLN_CTL, QUOT_CTRL, \
-    SFT_CAPS, KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,   KC_LBRC,  KC_RBRC, 	KC_N, 	KC_M, 	KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+    SFT_CAPS, KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,   KC_LBRC,  KC_RBRC, 	KC_N, 	KC_M, 	KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT, \
    						KC_LALT, KC_LGUI, TT(1), KC_ENT, 			KC_SPC, KC_BSPC, TT(2), KC_EQL \
  ),
 /* LOWER
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define LEFT_CMD     GUI_T(KC_LEFT)
 #define DOWN_ALT     ALT_T(KC_DOWN)
 #define RIGHT_SFT    SFT_T(KC_RGHT)
-#define END_CTL    CTL_T(KC_END)
+#define END_CTL      CTL_T(KC_END)
 
  [_LOWER] = LAYOUT(
 	F1_ESC,    KC_F2, 	  KC_F3, 	 KC_F4, 	KC_F5, 	   KC_F6, 						   KC_F7, 	  KC_F8, 	 KC_F9, 	 KC_F10,     KC_F11,    KC_F12, \
