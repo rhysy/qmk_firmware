53f371bb357ccd3853a823b77d46f4d71f98cbcb
Rhys Yamada <rhys.yamada@gmail.com>
2019-12-03 22:30:01 -1000

    Added one shot modifier, formatted visual keymap

---
+++ keyboards/dztech/dz65rgb/keymaps/rhysy/config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
