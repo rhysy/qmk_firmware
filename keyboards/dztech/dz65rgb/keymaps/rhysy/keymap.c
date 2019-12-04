34de701ca7dc1ce85a76f75ab4d3616f2dbd07b6
ryamada <ryamada@switchfly.com>
2019-12-03 20:07:28 -1000

    Visual keymap, layer1

--- keyboards/dztech/dz65rgb/keymaps/rhysy/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/rhysy/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,  KC_RBRC, KC_BSLS, KC_DEL,\
     CTRL_CAPS,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,     KC_ENT,     KC_PGUP, \
     KC_LSFT,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,   KC_RSFT,  KC_UP,    KC_PGDN, \
-    KC_LCTL,         KC_LALT, KC_LGUI,                 KC_SPC,                KC_RGUI,  MO(1), KC_RALT, KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LCTL,  KC_LALT,  KC_LGUI,                    KC_SPC,                   KC_RGUI,  MO(1), KC_RALT, KC_LEFT,  KC_DOWN,  KC_RGHT
    ),
 
  /* LAYER 1
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+----------+-----|
   * |        |RGBTG| MD+ | HUE+| HUE-| SAT+| SAT-| BRT+| BRT-|     |PTSRN|SC LK|PAUSE| RESET |     |
   * |--------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+-----|
-  * |          | SPD+| SPD-|     |     |     |     |     |     |     |     |     | EEPROM RST|     |
+  * |          | SPD+| SPD-|     |     |     |     |     |     |     |     |     | EEPROM RST| PLAY|
   * |----------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----------+-----|
   * |            |     |     |     |     |     |     |     |     |     |     |         | VOL+| MUTE|
   * |------------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+---------+-----+-----|
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS,  RESET,   KC_TRNS, \
     CTRL_TRNS,    RGB_SPI, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     EEP_RST,     KC_MPLY, \
     KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_VOLU, KC_MUTE, \
-    KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT
+    KC_TRNS,  KC_TRNS,  KC_TRNS,                        KC_TRNS,                         KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT
     )
 };
 
