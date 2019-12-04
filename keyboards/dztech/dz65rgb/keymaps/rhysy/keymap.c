024487be5226ffa5488d972b2c223ac48ee0c192
ryamada <ryamada@switchfly.com>
2019-12-03 20:03:45 -1000

    Visual keymap, layer1

--- keyboards/dztech/dz65rgb/keymaps/rhysy/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/rhysy/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  /* LAYER 1
   * ,----------------------------------------------------------------------------------------------.
-  * |     |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |          |     |
+  * |     |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   DEL    |     |
   * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+----------+-----|
   * |        |RGBTG| MD+ | HUE+| HUE-| SAT+| SAT-| BRT+| BRT-|     |PTSRN|SC LK|PAUSE| RESET |     |
   * |--------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-------+-----|
@@ -38,7 +38,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |      |      |      |                                     |     |     |     | PREV| VOL-| MEXT|
   * '----------------------------------------------------------------------------------------------'
  */
-	[1] = LAYOUT_65_ansi(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_TRNS, KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, RESET, KC_TRNS, LCTL_T(KC_TRNS), RGB_SPI, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EEP_RST, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT)
+#define CTRL_TRNS   LCTL_T(KC_TRNS)
+  [1] = LAYOUT_65_ansi(
+    KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL,   KC_TRNS, \
+    KC_TRNS,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS,  RESET,   KC_TRNS, \
+    CTRL_TRNS,    RGB_SPI, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     EEP_RST,     KC_MPLY, \
+    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_VOLU, KC_MUTE, \
+    KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT
+    )
 };
 
 void rgb_matrix_indicators_user(void) {
