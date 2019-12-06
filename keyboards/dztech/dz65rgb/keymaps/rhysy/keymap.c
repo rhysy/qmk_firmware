6836c05df2e4a109c62cdbe91992f25f3a1d7e4e
ryamada <ryamada@switchfly.com>
2019-12-05 16:20:33 -1000

    Added 3rd layer to modify RGB. Added layer color changes. Added capslock color change.

--- keyboards/dztech/dz65rgb/keymaps/rhysy/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/rhysy/keymap.c
@@ -40,7 +40,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [1] = LAYOUT_65_ansi(
-    KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL,   KC_TRNS, \
+    KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL,   TO(2), \
+    KC_TRNS,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS,  RESET,   KC_TRNS, \
+    KC_TRNS,      RGB_SPI, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     EEP_RST,     KC_MPLY, \
+    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_VOLU, KC_MUTE, \
+    KC_TRNS,  KC_TRNS,  KC_TRNS,                        KC_TRNS,                         KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT
+    ),
+
+
+
+ /* LAYER 1
+  * ,-----,-----,-----,-----,-----,-----,-----,-----,-----,-----,-----,-----,-----,----------,-----.
+  * |     |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   DEL    |     |
+  * |-----'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,-------+-----|
+  * |        |RGBTG| MD+ | HUE+| HUE-| SAT+| SAT-| BRT+| BRT-|     |PTSRN|SC LK|PAUSE| RESET |     |
+  * |--------'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-------+-----|
+  * |          | SPD+| SPD-|     |     |     |     |     |     |     |     |     | EEPROM RST| PLAY|
+  * |----------'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-----,-----+-----|
+  * |            |     |     |     |     |     |     |     |     |     |     |         | VOL+| MUTE|
+  * |------,-----',----'-,---'-----'-----'-----'-----'-----'---,-'---,-'---,-'---,-----'-----+-----|
+  * |      |      |      |                                     |     |     |     | PREV| VOL-| MEXT|
+  * '------'------'------'-------------------------------------'-----'-----'-----'-----'-----'-----'
+ */
+
+  [2] = LAYOUT_65_ansi(
+    KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL,   TO(0), \
     KC_TRNS,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS,  RESET,   KC_TRNS, \
     KC_TRNS,      RGB_SPI, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     EEP_RST,     KC_MPLY, \
     KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_VOLU, KC_MUTE, \
@@ -48,8 +72,71 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+int rgb_current_mode;
+HSV current_hsv;
+
+void matrix_init_user(void) {
+    #ifdef RGB_MATRIX_ENABLE
+        //  rgb_matrix_config.raw = eeprom_read_dword(EECONFIG_RGB_MATRIX);
+        rgb_current_mode = rgb_matrix_config.mode;
+        current_hsv = rgb_matrix_config.hsv;
+    #endif
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+      switch (keycode) {
+            case RGB_MOD:
+            case RGB_HUI:
+            case RGB_HUD:
+            case RGB_SAI:
+            case RGB_SAD:
+            case RGB_VAI:
+            case RGB_VAD:
+                if (record->event.pressed) {}
+                else {
+                    rgb_current_mode = rgb_matrix_config.mode;
+                    current_hsv = rgb_matrix_config.hsv;
+                }
+      }
+      return true;
+}
+
+void rgb_matrix_layer_helper (uint8_t hue, uint8_t sat, uint8_t val) {
+    HSV hsv = {hue, sat, val};
+    RGB rgb = hsv_to_rgb(hsv);
+    for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
+        // if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_MODIFIER)) { Only changes the modifier key colors
+        rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+    }
+}
+
+int last_layer_state;
+
 void rgb_matrix_indicators_user(void) {
-    if (IS_LED_ON(host_keyboard_leds(), USB_LED_CAPS_LOCK)) {
-        rgb_matrix_set_color(8, 0xFF, 0xFF, 0xFF);
+    if (!g_suspend_state) {
+        if (IS_LED_ON(host_keyboard_leds(), USB_LED_CAPS_LOCK)) {
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+            rgb_matrix_layer_helper(HSV_MAGENTA);
+            rgb_matrix_set_color(8, 0xFF, 0xFF, 0xFF);
+        } else {
+            switch(biton32(layer_state)) {
+                case 0:
+                    // Default layer will use whatever is in EEPROM
+                    rgb_matrix_mode_noeeprom(rgb_current_mode);
+                    break;
+                case 1:
+                    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+                    rgb_matrix_layer_helper(HSV_CYAN);
+                    break;
+                case 2: // RGB mod layer
+                    if (last_layer_state != 2) {
+                        // Set layer to default layer's profile to allow for modification
+                        rgb_matrix_mode_noeeprom(rgb_current_mode);
+                    }
+                    break;
+            }
+        }
+
+        last_layer_state = biton32(layer_state);
     }
 }
