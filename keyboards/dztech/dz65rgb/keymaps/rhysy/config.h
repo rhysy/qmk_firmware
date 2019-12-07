80767d29d705340bd4bee7ea68d9a704dd2281f0
ryamada <ryamada@switchfly.com>
2019-12-06 17:23:12 -1000

    Increased brightness

--- keyboards/dztech/dz65rgb/keymaps/rhysy/config.h
+++ keyboards/dztech/dz65rgb/keymaps/rhysy/config.h
@@ -2,3 +2,6 @@
 
 #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+
+#undef  RGB_MATRIX_MAXIMUM_BRIGHTNESS
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 220
