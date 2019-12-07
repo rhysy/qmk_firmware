1efef10939f50f7d1f4f006a1a2279b5fc56c575
ryamada <ryamada@switchfly.com>
2019-12-06 19:10:04 -1000

    Decrease the brightness step

--- keyboards/dztech/dz65rgb/keymaps/rhysy/config.h
+++ keyboards/dztech/dz65rgb/keymaps/rhysy/config.h
@@ -3,5 +3,6 @@
 #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
 
+#define RGB_MATRIX_VAL_STEP 6
 #undef  RGB_MATRIX_MAXIMUM_BRIGHTNESS
 //#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 220
