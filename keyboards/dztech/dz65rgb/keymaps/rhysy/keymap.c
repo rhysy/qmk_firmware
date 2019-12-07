b117c182ca53cf1bf0c8e8538d729f9990ad7ef2
ryamada <ryamada@switchfly.com>
2019-12-06 18:01:45 -1000

    Remap media keys

--- keyboards/dztech/dz65rgb/keymaps/rhysy/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/rhysy/keymap.c
@@ -33,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,-----,-----,-----,-----,-----,-----,-----,-----,-----,-----,-----,-----,-----,----------,-----.
   * |     |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   DEL    |     |
   * |-----'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,--'--,-------+-----|
-  * |        |     |     |     |     |     |     |     |     |     |     |     |     |       |     |
+  * |        |     |     |     |     |     |     |     |     |     | PLAY|     |     |       |     |
   * |--------'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-------+-----|
-  * |          |     |     |     |     |     |     |     |     |     |     |     |           | PLAY|
+  * |          |     |     |     |     |     |     |     |     |     |     |     |           |     |
   * |----------'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-,---'-----,-----+-----|
-  * |            |     |     |     |     |     |     |     |     |     |     |         | VOL+| MUTE|
+  * |            |     |     |     |     |     |     | MUTE|     |     |     |         | VOL+|     |
   * |------,-----',----'-,---'-----'-----'-----'-----'-----'---,-'---,-'---,-'---,-----'-----+-----|
   * |      |      |      |                                     |     |     |     | PREV| VOL-| MEXT|
   * '------'------'------'-------------------------------------'-----'-----'-----'-----'-----'-----'
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FUNC] = LAYOUT_65_ansi(
     KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL,   TO(2), \
-    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, \
-    KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,     KC_MPLY, \
-    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_VOLU, KC_MUTE, \
+    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, \
+    KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS, \
+    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_VOLU, KC_TRNS, \
     KC_TRNS,  KC_TRNS,  KC_TRNS,                        KC_TRNS,                         KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT
     ),
 
