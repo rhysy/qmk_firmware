fd6fa316283dcb43c7db6378cc3cd36f821a11c2
Rhys Yamada <rhys.yamada@gmail.com>
2020-08-01 11:48:08 -1000

    Change GUI mods to CTRL mods

--- keyboards/boardwalk/keymaps/rhysy/keymap.c
+++ keyboards/boardwalk/keymaps/rhysy/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
      * | LSHIFT | Z      | X      | C      | V      | B      | END    | PG DN  | N      | M      | ,      | .      | /      | RSHIFT |
      * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
-     * | LCTRL  | LGUI   | LALT   | LGUI   | LOWER  | SPACE  | SPACE  | SPACE  | SPACE  | RAISE  | RGUI   | RALT   | RGUI   | RCTRL  |
+     * | LCTRL  | LCTRL  | LALT   | LGUI   | LOWER  | SPACE  | SPACE  | SPACE  | SPACE  | RAISE  | RGUI   | RALT   | RCTRL  | RCTRL  |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
     [_BASE] = LAYOUT_ortho_5x14(
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_LGUI, OSL(_LW),KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  MO(_HI), KC_RGUI, KC_RALT, KC_RGUI, KC_RCTL
+        KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, OSL(_LW),KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  MO(_HI), KC_RGUI, KC_RALT, KC_RCTL, KC_RCTL
     ),
 
     /* LOWER (fn) Layer
